fd33af25f94e48fc62451e407490c84f63e0f2e9
arieltecay <arieltecay@gmail.com>
2025-11-14 18:51:08 -0300

    add analitics and insight data of vercel

--- src/main.tsx
+++ src/main.tsx
@@ -4,12 +4,16 @@ import './index.css';
 import App from './App.tsx';
 import { ThemeProvider } from './context/ThemeContext';
 import { LanguageProvider } from './context/LanguageContext';
+import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/react"
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <ThemeProvider>
       <LanguageProvider>
         <App />
+        <Analytics />
+        <SpeedInsights />
       </LanguageProvider>
     </ThemeProvider>
   </StrictMode>,
